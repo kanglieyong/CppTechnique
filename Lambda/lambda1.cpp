40c180b922d74a8c34ce1e0144b892d84db7e835
kly <kanglieyong@gmail.com>
2015-04-05 22:11:49 +0800

    lambda sample

---
+++ Lambda/lambda1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+
+const long Size = 390000L;
+
+int main()
+{
+  using std::cout;
+  using std::endl;
+  using std::vector;
+
+  vector<int> numbers(Size);
+
+  std::srand(std::time(0));
+  std::generate(numbers.begin(), numbers.end(), std::rand);
+  cout << "Sample size = " << Size << "\n";
+
+  // using lambdas
+  int count3 = std::count_if(numbers.begin(), numbers.end(),
+			     [](int x){ return x % 3 == 0; });
+  cout << "Count of numbers divisible by 3 : " << count3 << endl;
+}
