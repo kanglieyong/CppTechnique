c9627a7b8b6aa0e45ef89ba4439cd8f38f3a8c80
kly <kanglieyong@gmail.com>
2015-04-07 05:15:24 +0800

    partInsertionSort

--- PartInsertionSort/partInsertionSort.h
+++ PartInsertionSort/partInsertionSort.h
@@ -3,12 +3,15 @@
 using std::vector;
 
 template <typename Comparable>
-void partInsertionSort(vector<Comparable>& a, int left, int right)
+void insertionSort(vector<Comparable>& a, int left, int right)
 {
   int j;
 
   for (int p = 1; p < right - left + 1; p++) {
-    Comparable tmp = a[left + 1];
-    for (j = left + p; j > 0)
+    Comparable tmp = a[left + p];
+    for (j = p; j - p > 0 && tmp < a[j - 1]; j--) {
+      a[j] = a[j - 1];
+    }
+    a[j] = tmp;
   }
 }
