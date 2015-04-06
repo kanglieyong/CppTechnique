a3fd0b5c9bc92c9039c27db6e61bffc9d7bfde57
kly <kanglieyong@gmail.com>
2015-04-07 04:08:41 +0800

    some bugs

--- PartInsertionSort/partInsertionSort.h
+++ PartInsertionSort/partInsertionSort.h
@@ -5,5 +5,10 @@ using std::vector;
 template <typename Comparable>
 void partInsertionSort(vector<Comparable>& a, int left, int right)
 {
-  //
+  int j;
+
+  for (int p = 1; p < right - left + 1; p++) {
+    Comparable tmp = a[left + 1];
+    for (j = left + p; j > 0)
+  }
 }
