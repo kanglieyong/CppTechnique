62d0c1e7443f6b75fc492c398b3229e5169d49a4
kanglieyong <kanglieyong@gmail.com>
2015-05-23 01:28:19 +0800

    change the implementation file

--- RoundRobin/RoundRobin.hpp
+++ RoundRobin/RoundRobin.hpp
@@ -1,4 +1,30 @@
-#include "RoundRobin.h"
+#ifndef ROUNDROBIN_H_
+#define ROUNDROBIN_H_
+
+#include <vector>
+#include <stdexcept>
+
+template <typename T>
+class RoundRobin
+{
+public:
+  RoundRobin(int numExpected = 0);
+  virtual ~RoundRobin();
+
+  void add(const T& elem);
+  void remove(const T& elem);
+  T& getNext() throw (std::out_of_range);
+  
+protected:
+  std::vector<T> mElems;
+  typename std::vector<T>::iterator mCurElem;
+  
+private:
+  RoundRobin(const RoundRobin& src) {};
+  RoundRobin& operator=(const RoundRobin& rhs) {};
+};
+
+#endif
 
 template <typename T>
 RoundRobin<T>::RoundRobin(int numExpected)
@@ -8,8 +34,7 @@ RoundRobin<T>::RoundRobin(int numExpected)
 
 template <typename T>
 RoundRobin<T>::~RoundRobin()
-{
-}
+{}
 
 template <typename T>
 void RoundRobin<T>::add(const T& elem)
@@ -25,15 +50,15 @@ void RoundRobin<T>::remove(const T& elem)
   for (auto it = mElems.begin(); it != mElems.end(); ++it) {
     if (*it == elem) {
       int newPos;
-      if (mCurElem <= it) {
-	newPos = mCurElem - mElems.begin();
-      } else {
-	newPos = mCurElem - mElems.begin() - 1;
-      }
+      if (mCurElem <= it) newPos = mCurElem - mElems.begin();
+      else                newPos = mCurElem - mElems.begin() - 1;
+
       mElems.erase(it);
+
       mCurElem = mElems.begin() + newPos;
       if (mCurElem == mElems.end()) mCurElem = mElems.begin();
-      return;
+
+      return ;
     }
   }
 }
@@ -44,6 +69,7 @@ T& RoundRobin<T>::getNext() throw (std::out_of_range)
   if (mElems.empty()) throw std::out_of_range("No element in the list");
 
   T& retVal = *mCurElem;
+
   ++mCurElem;
   if (mCurElem == mElems.end()) mCurElem = mElems.begin();
 
