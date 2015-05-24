5e0388b9681909adb25d0562eac3412d81cb0f09
kanglieyong <kanglieyong@gmail.com>
2015-05-24 22:48:12 +0800

    add some TEST SAMPLES

--- SuperSmartPointer/testSuperSmartPointer.cpp
+++ SuperSmartPointer/testSuperSmartPointer.cpp
@@ -1,5 +1,10 @@
 #include "SuperSmartPointer.hpp"
 #include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
 
 class Nothing
 {
@@ -23,9 +28,24 @@ int main(void)
   }
 
   if (Nothing::sNumAllocations != Nothing::sNumDeletions) {
-    std::cout << "TEST FAILED: " << Nothing::sNumAllocations << " allocations and " << Nothing::sNumDeletions << " deletions" << std::endl;
+    cout << "TEST FAILED: " << Nothing::sNumAllocations << " allocations and " << Nothing::sNumDeletions << " deletions" << endl;
   } else {
-    std::cout << "TEST PASSED" << std::endl;
+    cout << "TEST PASSED" << endl;
+  }
+
+  {
+    SuperSmartPointer<string> ptr3(new string("Hello, World!"));
+    SuperSmartPointer<string> ptr4(ptr3);
+    if (*ptr3 == *ptr4) cout << "TEST PASSED" << endl;
+  }
+
+  {
+    SuperSmartPointer<string> ptr5(new string("Dennis Ritchie"));
+    SuperSmartPointer<string> ptr6(new string("John Donne"));
+    ptr6 = ptr5;
+    if (*ptr5 == *ptr6) cout << "Test PASSED" << endl;
+    cout << "*ptr5 = " << *ptr5 << ", *ptr6 = " << *ptr6 << endl;
+    cout << "ptr5 = " << ptr5 << ", ptr6 = " << ptr6 << endl;
   }
 
   return 0;
