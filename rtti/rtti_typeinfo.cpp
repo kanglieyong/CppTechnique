2318f64c1fdd71ed34ca8eb1afa9bd4732e943a7
kanglieyong <kanglieyong@gmail.com>
2015-10-04 00:16:56 +0800

    typeinfo

---
+++ rtti/rtti_typeinfo.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <typeinfo>
+
+using std::cout;
+
+class Grand
+{
+private:
+  int hold;
+  
+public:
+  Grand(int h = 0)
+    : hold(h) {}
+
+  virtual void Speak() const { cout << "I am a grand class!\n"; }
+  virtual int Value() const { return hold; }
+};
+
+class Superb : public Grand
+{
+public:
+  Superb(int h = 0)
+    : Grand(h) {}
+
+  void Speak() const { cout << "I am a superb class!!!\n"; }
+  virtual void Say() const { cout << "I hold the superb value of " << Value() << "!\n"; }
+};
+
+class Magnificent : public Superb
+{
+private:
+  char ch;
+public:
+  Magnificent(int h = 0, char c = 'A')
+    : Superb(h), ch(c) {}
+
+  void Speak() const { cout << "I am a magnificent class!!!\n"; }
+  void Say() const { cout << "I hold the character " << ch << " and the integer " << Value() << "!\n";}
+};
+
+Grand* getOne();
+
+int main(void)
+{
+  std::vector<Grand*> pvec;
+  std::srand(std::time(0));
+  Grand* pg;
+  Superb* ps;
+  for (int i = 0; i < 5; i++) {
+    pg = getOne();
+    pvec.push_back(pg);
+    cout << "Now processing type " << typeid(*pg).name() << ".\n";
+    pg->Speak();
+    if (ps = dynamic_cast<Superb *>(pg)) {
+      ps->Say();
+    }
+    if (typeid(Magnificent) == typeid(*pg)) {
+      cout << "Yes, You're really magnificent.\n";
+    }
+    cout << "\n";
+  }
+
+  for (int i = 0; i < pvec.size(); ++i) {
+    delete pvec[i];
+  }
+  pvec.clear();
+
+  return 0;
+}
+
+Grand* getOne()
+{
+  Grand* p;
+  switch (rand() % 3) {
+  case 0:
+    p = new Grand(rand() % 100);
+    break;
+  case 1:
+    p = new Superb(rand() % 100);
+    break;
+  case 2:
+    p = new Magnificent(rand()%100, rand()%26 + 'A');
+    break;
+  }
+}
